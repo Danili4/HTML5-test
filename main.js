3901db8f49900a8acfbb5546586ef1b196312f43
Danili4 <daniil.filippenko@gmail.com>
2014-08-26 13:05:52 +0400

    serve static

--- main.js
+++ main.js
@@ -1,25 +1,53 @@
 var http = require("http")
-var mustache = require("mustache")
+var fs = require("fs")
+var path = require("path")
+var mime = require("mime")
+var cache = {}
 
-var view = {
-	title: "Jojij",
-	calc: function () {
-		return 5 * 4
-	}
-}
-
-var template = "<body><h3>{{title}}</h3><p> <strong>spend</strong> {{calc}}</p></body>"
-
-var output = mustache.render(template, view)
-
-var router = function (request, response) {
-	response.writeHead(200, {"Content-type": "text/html"})
-	response.write(output)
+function send404(response) {
+	response.writeHead(404, {"Content-type": "text/plain"})
+	response.write("Error 404: resource not found.")
 	response.end()
 }
 
-console.log(output)
+function sendFile(response, filePath, fileContents) {
+	response.writeHead(200, {"Content-type": mime.lookup(path.basename(filePath))})
+	response.end(fileContents)
+	console.log(filePath)
+}
 
-var server = http.createServer(router)
+function serveStatic(response, cache, absPath) {
+	if(cache[absPath]) {
+		sendFile(response, absPath, cache[absPath])
+	} else {
+		fs.exists(absPath, function(exists) {
+			if(exists) {
+				fs.readFile(absPath, function(err, data) {
+					if(err) {
+						send404(response)
+					} else {
+						cache[absPath]=data
+						sendFile(response, absPath, data)
+					}
+				})
+			} else {
+				send404(response)
+			}	
+		})
+	}
+}
+
+function route(request, response) {
+var filePath = false
+if(request.url =='/') {
+	filePath = 'public/index.html'
+} else {
+	filePath = 'public' + request.url
+}
+
+var absPath = './' + filePath
+serveStatic(response, cache, absPath)
+}
+var server = http.createServer(route)
 server.listen(3099)
 console.log("server listen at 3099 port")
